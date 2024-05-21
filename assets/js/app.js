ea0499e6093ef3a0a37feaa3a52ba49d525cf862
AyxanAmirov <amirovayxan0@gmail.com>
2024-05-21 12:29:46 -0700

    forms update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,19 @@ const parentImage = document.querySelector(".slider-image-parent");
 const childImages = document.querySelectorAll(".slider-image-child");
 const slideName = document.querySelector(".room-name");
 const filterForm = document.querySelector(".filter");
-const checkIn = document.querySelector("#checkIn");
-const checkOut = document.querySelector("#checkOut");
-const inRequired = document.querySelector("#reqIn");
-const OutRequired = document.querySelector("#reqOut");
+const checkInInput = document.getElementById("checkIn");
+const checkOutInput = document.getElementById("checkOut");
+const checkInError = document.getElementById("reqIn");
+const checkInValid = document.getElementById("validIn");
+const checkOutError = document.getElementById("reqOut");
+const checkOutValid = document.getElementById("validOut");
+const nameInput = document.getElementById("nameInput");
+const emailInput = document.getElementById("emailInput");
+const messageInput = document.getElementById("messageInput");
+const nameRegister = document.getElementById("nameRegister");
+const emailRegister = document.getElementById("emailRegister");
+const messageRegister = document.getElementById("messageRegister");
+const emailValid = document.getElementById("emailValid");
 function changeSlider(img) {
   parentImage.src = img;
   childImages.forEach((image, i) => {
@@ -31,21 +40,117 @@ function changeSlider(img) {
     }
   });
 }
+let isValid = true;
 
 filterForm.addEventListener("submit", (e) => {
-  if (checkIn.value.trim() == "" && checkOut.value.trim() == "") {
-    inRequired.className = "required";
-    OutRequired.className = "required";
-  } else if (checkIn.value.trim() == "") {
-    inRequired.className = "required";
-  } else if (checkOut.value.trim() == "") {
-    OutRequired.className = "required";
+  let datePattern = /^\d{2}\d{2}\d{4}$/;
+
+  if (checkInInput.value.trim() == "") {
+    checkInError.className = "required";
   } else {
-    OutRequired.className = "d-none";
-    checkOut.value = "";
-    inRequired.className = "d-none";
-    checkIn.value = "";
+    if (datePattern.test(checkInInput.value)) {
+      checkInValid.className = "d-none";
+    } else {
+      checkInValid.className = "required";
+    }
+    checkInError.className = "d-none";
+  }
+  if (checkOutInput.value.trim() == "") {
+    checkOutError.className = "required";
+  } else {
+    if (datePattern.test(checkOutInput.value)) {
+      checkOutValid.className = "d-none";
+    } else {
+      checkOutValid.className = "required";
+    }
+    checkOutError.className = "d-none";
+  }
+
+  if (!checkInInput.value.trim() == "" && !checkOutInput.value.trim() == "") {
+    if (
+      datePattern.test(checkInInput.value) &&
+      datePattern.test(checkOutInput.value)
+    ) {
+      let checkObj = {
+        checkIn: checkInInput.value,
+        checkOut: checkOutInput.value,
+      };
+      fetch("https://6622d9c13e17a3ac846e1a5a.mockapi.io/test", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(checkObj),
+      });
+
+      checkInInput.value = "";
+      checkOutInput.value = "";
+    }
   }
 
   e.preventDefault();
 });
+
+(function () {
+  emailjs.init("5v5TsNF9LD7u5G8mr");
+})();
+
+function sendMail(e) {
+  e.preventDefault();
+
+  const emailForm = document.querySelector(".email-form");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (
+    !emailInput.value.trim() == "" &&
+    !nameInput.value.trim() == "" &&
+    !messageInput.value.trim() == ""
+  ) {
+    if (!emailRegex.test(emailInput.value)) {
+      emailValid.classList = "required";
+    } else {
+      emailjs
+        .sendForm(
+          "service_jkzzetq",
+          "template_wu648in",
+          emailForm,
+          "5v5TsNF9LD7u5G8mr"
+        )
+        .then(
+          (response) => {
+            console.log("SUCCESS!", response.status, response.text);
+          },
+          (error) => {
+            console.log("FAILED...", error);
+          }
+        );
+      emailValid.classList = "d-none";
+      emailInput.value = "";
+      nameInput.value = "";
+      messageInput.value = "";
+
+      return false;
+    }
+  }
+  // email
+  if (emailInput.value.trim() == "") {
+    emailRegister.className = "required";
+  } else {
+    if (!emailRegex.test(emailInput.value)) {
+      emailValid.classList = "required";
+    }
+    emailRegister.className = "d-none";
+  }
+  // name
+  if (nameInput.value.trim() == "") {
+    nameRegister.className = "required";
+  } else {
+    nameRegister.className = "d-none";
+  }
+  // message
+  if (messageInput.value.trim() == "") {
+    messageRegister.className = "required";
+  } else {
+    messageRegister.className = "d-none";
+  }
+}
